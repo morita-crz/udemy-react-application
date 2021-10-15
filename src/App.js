9249bade3fd3ac5df8d57bd9daf2c19cc068df5e
morita-crz <kumiko.morita@careritz.co.jp>
2021-10-15 16:54:00 +0900

    action creater

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, {Component} from 'react';
 
+const App = () => (<Counter></Counter>)
 
-function App() {
-  return <div>Hello world!</div>;
+class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+  }
+
+  handlePlusBtn = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinBtn = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+      <div>counter: {this.state.count}</div>
+      <button onClick={this.handlePlusBtn}>+1</button>
+      <button onClick={this.handleMinBtn}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
